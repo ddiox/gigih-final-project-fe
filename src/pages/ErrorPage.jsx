a24357281d48cd643c0116d9feaaaccb60fe9274
Glenn <glenn.claudiox@gmail.com>
2023-08-16 04:08:56 +0800

    adding ErrorPage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Box, Center, Heading } from "@chakra-ui/react";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <Center h="100vh">
+      <Box>
+        <Heading as="h1" size="xl" textAlign="center">
+          Not Found
+        </Heading>
+      </Box>
+    </Center>
+  );
 };
 
 export default ErrorPage;
